2951d4795eeace22255b48f581c1b32839df3a39
Anna <119071299+heyanja@users.noreply.github.com>
2024-05-19 12:58:55 -0400

    Initial Commit

---
+++ Mimicry/MimicryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimicryApp.swift
+//  Mimicry
+//
+//  Created by Anna Fadieieva on 2024-05-19.
+//
+
+import SwiftUI
+
+@main
+struct MimicryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
